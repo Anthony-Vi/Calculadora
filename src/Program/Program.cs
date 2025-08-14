8c5a0e5aa8dc3c33ee8c73eed8b375d85b7f7b37
Ezequiel Pasto <ezequiel.pastorino@correo.ucu.edu.uy>
2025-08-14 12:05:27 -0300

    agregar

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,6 +2,14 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+
+
+
+
+
+
+
+
 public class Division
 {
     public static double Divide(int a, int b)
