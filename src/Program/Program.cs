b676e1bf3ff68e3f3d5d5243712d10cc77cbf1cf
Ezequiel Pasto <ezequiel.pastorino@correo.ucu.edu.uy>
2025-08-14 16:13:44 -0300

    cambio

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,13 +3,6 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-
-
-
-
-
-
-
 public class Division
 {
     public static double Divide(int a, int b)
@@ -17,4 +10,5 @@ public class Division
         return (double)a / b;
     }
 }
+
 
