8c2655173ac8310d9b3171078b072776278daa42
Andres <ANDRES.RODRIGUEZC@correo.ucu.edu.uy>
2025-08-19 10:44:38 -0300

    JJAJAJA TROLEADO2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,18 +3,38 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-public class Multiplication
+public class Addition
+    //esta clase implementa la operacion suma
 {
-    public static int Multiply(int a, int b)
+    public static int Add(int a, int b)
     {
-        return a * b;
+        return a + b;
+    }
+}
+public class Division
+
+{
+    public static double Divide(int a, int b)
+        //Esta clase implementa la operacion division
+    {
+        return (double)a / b;
     }
 }
 
 public class Substraction
+// esta clase implementa la operacion resta
 {
     public static int Substract(int a, int b)
     {
         return a - b;
+    }
+}
+
+public class Multiplication
+// esta clase implimenta la operacion mulitplicacion
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
     }
 }
